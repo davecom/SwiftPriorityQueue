a6ae2f53d6571664f72740deffdd726751353785
Peter Gao <dapenggao@users.noreply.github.com>
2015-05-10 18:37:45 +0800

    Update SwiftPriorityQueue.swift

--- SwiftPriorityQueue/SwiftPriorityQueue.swift
+++ SwiftPriorityQueue/SwiftPriorityQueue.swift
@@ -29,61 +29,45 @@
 /// can be accomplished in O(lg n) time. It can be specified whether the objects should
 /// be popped in ascending or descending order (Max Priority Queue or Min Priority Queue)
 /// at the time of initialization.
-class PriorityQueue<T: Comparable>: Printable, GeneratorType, SequenceType, CollectionType {
-    private final var heap: [T] = []
-    private let contrast: (T, T) -> Bool
+struct PriorityQueue<T: Comparable> {
     
-    convenience init() {
-        self.init(ascending: false, startingValues: [])
-    }
+    private var heap = [T]()
+    private let ordered: (T, T) -> Bool
     
-    convenience init(ascending: Bool) {
-        self.init(ascending: ascending, startingValues: [])
-    }
-    
-    convenience init(startingValues: [T]) {
-        self.init(ascending: false, startingValues: startingValues)
-    }
-    
-    init(ascending: Bool, startingValues: [T]) {
+    init(ascending: Bool = false, startingValues: [T] = []) {
+        
         if ascending {
-            contrast = {$0 > $1}
+            ordered = { $0 > $1 }
         } else {
-            contrast = {$0 < $1}
+            ordered = { $0 < $1 }
         }
         
-        for value in startingValues {
-            push(value)
-        }
+        for value in startingValues { push(value) }
     }
     
-    /// How many elements are in the Priority Queue?
-    var count: Int {
-        return heap.count
-    }
+    /// How many elements the Priority Queue stores
+    var count: Int { return heap.count }
     
-    /// Are there any elements in the Priority Queue?
-    var isEmpty: Bool {
-        return heap.isEmpty
-    }
+    /// true if and only if the Priority Queue is empty
+    var isEmpty: Bool { return heap.isEmpty }
     
     /// Add a new element onto the Priority Queue. O(lg n)
     ///
     /// :param: element The element to be inserted into the Priority Queue.
-    func push(element: T) {
+    mutating func push(element: T) {
         heap.append(element)
-        swim((heap.count - 1))
+        swim(heap.count - 1)
     }
     
     /// Remove and return the element with the highest priority (or lowest if ascending). O(lg n)
     ///
     /// :returns: The element with the highest priority in the Priority Queue, or nil if the PriorityQueue is empty.
-    func pop() -> T? {
-        if heap.isEmpty {
-            return nil;
-        }
-        swap(&heap[0], &heap[(heap.count - 1)])
-        let temp: T = heap.removeLast()
+    mutating func pop() -> T? {
+        
+        if heap.isEmpty { return nil }
+        
+        swap(&heap[0], &heap[heap.count - 1])
+        let temp = heap.removeLast()
         sink(0)
         
         return temp
@@ -93,74 +77,63 @@ class PriorityQueue<T: Comparable>: Printable, GeneratorType, SequenceType, Coll
     ///
     /// :returns: The element with the highest priority in the PriorityQueue, or nil if the PriorityQueue is empty.
     func peek() -> T? {
-        if heap.isEmpty {
-            return nil;
-        }
-        return heap[0]
+        return heap.first
     }
     
     /// Eliminate all of the elements from the Priority Queue.
-    func clear() {
+    mutating func clear() {
         heap.removeAll(keepCapacity: false)
     }
     
     // Based on example from Sedgewick p 316
-    private func sink(index: Int) {
-        var k: Int = index
-        while (((2 * k) + 1) < heap.count) {
-            var j: Int = (2 * k) + 1
-            if j < (heap.count - 1) && contrast(heap[j], heap[(j + 1)]) {
-                j++
-            }
-            if !contrast(heap[k], heap[j]) {
-                break;
-            }
-            swap(&heap[k], &heap[j])
-            k = j
+    private mutating func sink(var index: Int) {
+        
+        while 2 * index + 1 < heap.count {
+            
+            var j = 2 * index + 1
+            
+            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j++ }
+            if !ordered(heap[index], heap[j]) { break }
+            
+            swap(&heap[index], &heap[j])
+            index = j
         }
     }
     
     // Based on example from Sedgewick p 316
-    private func swim(index: Int) {
-        var k: Int = index
-        while k > 0 && contrast(heap[((k - 1) / 2)], heap[k]) {
-            swap(&heap[((k - 1) / 2)], &heap[k])
-            k = ((k - 1) / 2)
+    private mutating func swim(var index: Int) {
+        
+        while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
+            swap(&heap[(index - 1) / 2], &heap[index])
+            index = (index - 1) / 2
         }
     }
+}
+
+extension PriorityQueue: GeneratorType {
     
-    //Implement Printable protocol
-    var description: String {
-        return heap.description
-    }
-    
-    //Implement GeneratorType
     typealias Element = T
-    func next() -> Element? {
-        if let e = pop() {
-            return e
-        }
-        return nil
-    }
+    mutating func next() -> Element? { return pop() }
+}
+
+extension PriorityQueue: SequenceType {
     
-    //Implement SequenceType
     typealias Generator = PriorityQueue
-    func generate() -> Generator {
-        return self
-    }
+    func generate() -> Generator { return self }
+}
+
+extension PriorityQueue: CollectionType {
     
-    //Implement CollectionType
     typealias Index = Int
     
-    var startIndex: Int {
-        return 0
-    }
+    var startIndex: Int { return heap.startIndex }
+    var endIndex: Int { return heap.endIndex }
     
-    var endIndex: Int {
-        return count
-    }
+    subscript(i: Int) -> T { return heap[i] }
+}
+
+extension PriorityQueue: Printable, DebugPrintable {
     
-    subscript(i: Int) -> T {
-        return heap[i]
-    }
+    var description: String { return heap.description }
+    var debugDescription: String { return heap.debugDescription }
 }
