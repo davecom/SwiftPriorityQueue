f60f17dbe762a1627caccbc9ba67fd69ae128c9a
Dave <snpukwana@gmail.com>
2017-09-21 23:27:06 -0400

    Decoupled Xcode testing from the GUI sample app; Closes #14

--- SwiftPriorityQueueTests/SwiftPriorityQueueTests.swift
+++ SwiftPriorityQueueTests/SwiftPriorityQueueTests.swift
@@ -3,10 +3,9 @@
 //  SwiftPriorityQueueTests
 //
 //  Created by David Kopec on 3/27/15.
-//  Copyright (c) 2015 Oak Snow Consulting. All rights reserved.
+//  Copyright (c) 2015-2017 Oak Snow Consulting. All rights reserved.
 //
 
-import Cocoa
 import XCTest
 import Foundation
 
