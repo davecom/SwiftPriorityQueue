ab22553a6aa6e39ff704c7327164d1a1705e99fc
Peter Aisher <peter.aisher@me.com>
2019-09-28 15:14:44 +0200

    decorate all functions `@inlinable` and change `private` and `fileprivate` properties to `@usableFromInline internal` switch to using pointer functions for `heapify`, `swim`, `sink`

--- Sources/SwiftPriorityQueue/SwiftPriorityQueue.swift
+++ Sources/SwiftPriorityQueue/SwiftPriorityQueue.swift
@@ -64,6 +64,7 @@ internal func swim<T>(_ heap: UnsafeMutablePointer<T>, index: Int, ordered: (T,T
 
 @inlinable
 internal func heapify<T>(_ heap: UnsafeMutablePointer<T>, length: Int, ordered: (T,T) -> Bool) {
+    // Based on "Heap construction" from Sedgewick p 323
     var i = length/2 - 1
     while i >= 0 {
         sink(heap, length: length, index: i, ordered: ordered)
@@ -78,9 +79,13 @@ internal func heapify<T>(_ heap: UnsafeMutablePointer<T>, length: Int, ordered: 
 /// at the time of initialization.
 public struct PriorityQueue<T: Comparable> {
     
-    fileprivate var heap = [T]()
-    private let ordered: (T, T) -> Bool
+    @usableFromInline
+    internal var heap = [T]()
     
+    @usableFromInline
+    internal let ordered: (T, T) -> Bool
+    
+    @inlinable
     public init(ascending: Bool = false, startingValues: [T] = []) {
         self.init(order: ascending ? { $0 > $1 } : { $0 < $1 }, startingValues: startingValues)
     }
@@ -90,45 +95,72 @@ public struct PriorityQueue<T: Comparable> {
     /// - parameter order: A function that specifies whether its first argument should
     ///                    come after the second argument in the PriorityQueue.
     /// - parameter startingValues: An array of elements to initialize the PriorityQueue with.
+    @inlinable
     public init(order: @escaping (T, T) -> Bool, startingValues: [T] = []) {
         ordered = order
         
-        // Based on "Heap construction" from Sedgewick p 323
         heap = startingValues
-        var i = heap.count/2 - 1
-        while i >= 0 {
-            sink(i)
-            i -= 1
+        // heapify using buffer pointer
+        heap.withUnsafeMutableBufferPointer { bufferPointer in
+            guard let heapPointer = bufferPointer.baseAddress else { return }
+            heapify(heapPointer, length: bufferPointer.count, ordered: ordered)
         }
+//        // heapify using array
+//        var i = heap.count/2 - 1
+//        while i >= 0 {
+//            _sink(i)
+//            i -= 1
+//        }
     }
     
     /// How many elements the Priority Queue stores
+    @inlinable
     public var count: Int { return heap.count }
     
     /// true if and only if the Priority Queue is empty
+    @inlinable
     public var isEmpty: Bool { return heap.isEmpty }
     
     /// Add a new element onto the Priority Queue. O(lg n)
     ///
     /// - parameter element: The element to be inserted into the Priority Queue.
+    @inlinable
     public mutating func push(_ element: T) {
         heap.append(element)
-        swim(heap.count - 1)
+        // swim with buffer pointer
+        heap.withUnsafeMutableBufferPointer { bufferPointer in
+            guard let heapPointer = bufferPointer.baseAddress else { return }
+            swim(heapPointer, index: bufferPointer.count - 1, ordered: ordered)
+        }
+//        // swim with array
+//        _swim(heap.count - 1)
     }
     
     /// Remove and return the element with the highest priority (or lowest if ascending). O(lg n)
     ///
     /// - returns: The element with the highest priority in the Priority Queue, or nil if the PriorityQueue is empty.
+    @inlinable
     public mutating func pop() -> T? {
         
         if heap.isEmpty { return nil }
-        if heap.count == 1 { return heap.removeFirst() }  // added for Swift 2 compatibility
-        // so as not to call swap() with two instances of the same location
-        heap.swapAt(0, heap.count - 1)
-        let temp = heap.removeLast()
-        sink(0)
         
-        return temp
+        // sink with buffer pointer
+        heap.withUnsafeMutableBufferPointer { bufferPointer in
+            guard let heapPointer = bufferPointer.baseAddress else { return }
+            heapPointer.swapAt(0, bufferPointer.count - 1)
+            sink(heapPointer, length: bufferPointer.count - 1, index: 0, ordered: ordered)
+        }
+        
+        return heap.removeLast()
+        
+//        // sink with array
+//        if heap.count == 1 { return heap.removeFirst() }  // added for Swift 2 compatibility
+//        // so as not to call swap() with two instances of the same location
+//        heap.swapAt(0, heap.count - 1)
+//        let temp = heap.removeLast()
+//        _sink(0)
+//
+//        return temp
     }
     
     
@@ -136,14 +168,25 @@ public struct PriorityQueue<T: Comparable> {
     /// Silently exits if no occurrence found.
     ///
     /// - parameter item: The item to remove the first occurrence of.
+    @inlinable
     public mutating func remove(_ item: T) {
         if let index = heap.firstIndex(of: item) {
-            heap.swapAt(index, heap.count - 1)
-            heap.removeLast()
-            if index < heap.count { // if we removed the last item, nothing to swim
-                swim(index)
-                sink(index)
+            heap.withUnsafeMutableBufferPointer { bufferPointer in
+                guard let heapPointer = bufferPointer.baseAddress else { return }
+                
+                heapPointer.swapAt(index, bufferPointer.count - 1)
+                swim(heapPointer, index: index, ordered: ordered)
+                sink(heapPointer, length: bufferPointer.count - 1, index: 0, ordered: ordered)
             }
+            heap.removeLast()
+            
+//            // remove using array
+//            heap.swapAt(index, heap.count - 1)
+//            heap.removeLast()
+//            if index < heap.count { // if we removed the last item, nothing to swim
+//                _swim(index)
+//                _sink(index)
+//            }
         }
     }
     
@@ -151,6 +194,7 @@ public struct PriorityQueue<T: Comparable> {
     /// Silently exits if no occurrence found.
     ///
     /// - parameter item: The item to remove.
+    @inlinable
     public mutating func removeAll(_ item: T) {
         var lastCount = heap.count
         remove(item)
@@ -163,44 +207,48 @@ public struct PriorityQueue<T: Comparable> {
     /// Get a look at the current highest priority item, without removing it. O(1)
     ///
     /// - returns: The element with the highest priority in the PriorityQueue, or nil if the PriorityQueue is empty.
+    @inlinable
     public func peek() -> T? {
         return heap.first
     }
     
     /// Eliminate all of the elements from the Priority Queue.
+    @inlinable
     public mutating func clear() {
         heap.removeAll(keepingCapacity: false)
     }
     
-    // Based on example from Sedgewick p 316
-    private mutating func sink(_ index: Int) {
-        var index = index
-        while 2 * index + 1 < heap.count {
-            
-            var j = 2 * index + 1
-            
-            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j += 1 }
-            if !ordered(heap[index], heap[j]) { break }
-            
-            heap.swapAt(index, j)
-            index = j
-        }
-    }
-    
-    // Based on example from Sedgewick p 316
-    private mutating func swim(_ index: Int) {
-        var index = index
-        while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
-            heap.swapAt((index - 1) / 2, index)
-            index = (index - 1) / 2
-        }
-    }
+//    // Based on example from Sedgewick p 316
+//    private mutating func _sink(_ index: Int) {
+//        var index = index
+//        while 2 * index + 1 < heap.count {
+//
+//            var j = 2 * index + 1
+//
+//            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j += 1 }
+//            if !ordered(heap[index], heap[j]) { break }
+//
+//            heap.swapAt(index, j)
+//            index = j
+//        }
+//    }
+//
+//    // Based on example from Sedgewick p 316
+//    private mutating func _swim(_ index: Int) {
+//        var index = index
+//        while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
+//            heap.swapAt((index - 1) / 2, index)
+//            index = (index - 1) / 2
+//        }
+//    }
 }
 
 // MARK: - GeneratorType
 extension PriorityQueue: IteratorProtocol {
     
     public typealias Element = T
+    
+    @inlinable
     mutating public func next() -> Element? { return pop() }
 }
 
@@ -208,6 +256,8 @@ extension PriorityQueue: IteratorProtocol {
 extension PriorityQueue: Sequence {
     
     public typealias Iterator = PriorityQueue
+    
+    @inlinable
     public func makeIterator() -> Iterator { return self }
 }
 
@@ -216,11 +266,16 @@ extension PriorityQueue: Collection {
     
     public typealias Index = Int
     
+    @inlinable
     public var startIndex: Int { return heap.startIndex }
+    
+    @inlinable
     public var endIndex: Int { return heap.endIndex }
     
+    @inlinable
     public subscript(i: Int) -> T { return heap[i] }
     
+    @inlinable
     public func index(after i: PriorityQueue.Index) -> PriorityQueue.Index {
         return heap.index(after: i)
     }
@@ -229,6 +284,9 @@ extension PriorityQueue: Collection {
 // MARK: - CustomStringConvertible, CustomDebugStringConvertible
 extension PriorityQueue: CustomStringConvertible, CustomDebugStringConvertible {
     
+    @inlinable
     public var description: String { return heap.description }
+    
+    @inlinable
     public var debugDescription: String { return heap.debugDescription }
 }
